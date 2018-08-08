0944f0f718ed6bc00caa73fd00e939697a67a13c
Marta <martasidor86@gmail.com>
2018-08-08 22:05:10 +0200

    eliminated mistakes in java, added action listener

--- app/src/main/java/com/example/marta/guessinggame/MainActivity.java
+++ app/src/main/java/com/example/marta/guessinggame/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,75 +26,84 @@ public class MainActivity extends AppCompatActivity {
         String message = "";
         try {
             int guess = Integer.parseInt(guessText);
-            if (guess < theNumber) {
+            if (guess < theNumber)
                 message = guess + " is too low. Try again";
-            } else if (guess > theNumber)
+            else if (guess > theNumber)
                 message = guess + " is too high. Try again";
-            else if (guess > theNumber) {
+            else if (guess > theNumber)
                 message = "Game Over! Let's play again!";
-            } else if (guess < theNumber) {
-                message = "Game Over! Let's play again!";
-
-        else
-                {
-                    message = guess + " is correct. You win after "
-
-                }
+            else {
+                message = guess + " is correct. You win after ";
+                newGame();
             }
-    catch(Exception e)
-            {
-                message = "Enter the whole number between 1 and 100";
-            }
-    finally{
-                lblOutput.setText(message);
-                txtGuess.requestFocus();
-                txtGuess.selectAll();
+        } catch (Exception e) {
+            message = "Enter the whole number between 1 and 100";
+        } finally {
+            lblOutput.setText(message);
+            txtGuess.requestFocus();
+            txtGuess.selectAll();
+        }
+    }
 
-            }
-            public void newGame () {
-                theNumber = (int) (Math.random() * 100 + 1);
+    public void newGame() {
+        theNumber = (int) (Math.random() * 100 + 1);
+    }
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            txtGuess = (EditText) findViewById(R.id.txtGuess);
+            // tym czyms łaczy sie elementy z GUI z javA,
+            // R jak resource bo andro to są rzeczy z  res (czyli xml)
+            btnGuess = (Button) findViewById(R.id.btnGuess);
+            lblOutput = (TextView) findViewById(R.id.lblOutput);
+            newGame();
+            btnGuess.setOnClickListener(new View.OnClickListener() {
                 @Override
-                protected void onCreate (Bundle savedInstanceState){
-                    super.onCreate(savedInstanceState);
-                    setContentView(R.layout.activity_main);
-                    txtGuess = (EditText) findViewById(R.id.txtGuess); // tym czyms łaczy sie elementy z GUI z javA,
-                    // R jak resource bo andro to są rzeczy z  res (czyli xml)
-                    btnGuess = (Button) findViewById(R.id.btnGuess);
-                    lblOutput = (TextView) findViewById(R.id.lblOutput);
-                    newGame();
-
-                    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-                    setSupportActionBar(toolbar);
-
-                    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-                    fab.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                                    .setAction("Action", null).show();
-                        }
-                    });
+                public void onClick(View v) {
+                    checkGuess();
                 }
+            });
 
+            txtGuess.setOnEditorActionListener(new TextView.OnEditorActionListener() {
                 @Override
-                public boolean onCreateOptionsMenu (Menu menu){
-                    // Inflate the menu; this adds items to the action bar if it is present.
-                    getMenuInflater().inflate(R.menu.menu_main, menu);
-                    return true;
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    return true; //false jest z automatu i powoduje zniknięcie klawiatury
+                    //POPRAWIC TO NIE DZIALa
                 }
+            });
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
 
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public boolean onOptionsItemSelected (MenuItem item){
-                    // Handle action bar item clicks here. The action bar will
-                    // automatically handle clicks on the Home/Up button, so long
-                    // as you specify a parent activity in AndroidManifest.xml.
-                    int id = item.getItemId();
-
-                    //noinspection SimplifiableIfStatement
-                    if (id == R.id.action_settings) {
-                        return true;
-                    }
-
-                    return super.onOptionsItemSelected(item);
+                public void onClick(View view) {
+                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
                 }
+            });
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu (Menu menu){
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected (MenuItem item){
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                return true;
             }
+
+            return super.onOptionsItemSelected(item);
+        }
+    }
